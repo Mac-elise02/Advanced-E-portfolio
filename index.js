3d6dc77bb80f0b546c174213c4a69f2f2a76a4b4
Maclairen Rogers <Mac.Elise2020@gmail.com>
2025-07-25 00:01:05 -0400

    this is a more professional e-portfolio

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
-// template id: template_6swr0gh
-// service id: service_ft6o1oo
-// user id: QvPpa_xVh8gJgI4u3
+let isModalOpen = false; 
+let contrastToggle = false; 
+const scaleFactor = 1 / 20; 
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape"); 
+  const x = event.clientX * scaleFactor; 
+  const y = event.clientY * scaleFactor; 
+  
+  for ( let i = 0; i < shapes.length; ++i) {
+    const isOdd = i % 2 !== 0; 
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle; 
+  if (contrastToggle) {
+    document.body.classList += " dark-theme"
+  }
+  else {
+    document.body.classList.remove("dark-theme")
+  }
+}
 
 function contact(event) {
   event.preventDefault();
   const loading = document.querySelector(".modal__overlay--loading");
   const success = document.querySelector(".modal__overlay--success");
-
+  loading.classList += " modal__overlay--visible"; 
   emailjs
     .sendForm(
       "service_ft6o1oo",
@@ -16,7 +38,7 @@ function contact(event) {
     )
     .then(() => {
       loading.classList.remove("modal__overlay visible");
-      success.classList += "modal__overlay--visible";
+      success.classList += " modal__overlay--visible";
     })
     .catch(() => {
       loading.classList.remove("modal__overlay--visible");
@@ -26,7 +48,6 @@ function contact(event) {
     });
 }
 
-let isModalOpen = false; 
 function toggleModal() {
   if (isModalOpen) {
     isModalOpen = false; 
@@ -35,3 +56,8 @@ function toggleModal() {
   isModalOpen = true; 
   document.body.classList += " modal--open"; 
 }
+
+
+// template id: template_6swr0gh
+// service id: service_ft6o1oo
+// user id: QvPpa_xVh8gJgI4u3
